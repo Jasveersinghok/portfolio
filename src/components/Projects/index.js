afa7fe3c9bbe0404b122ea21cb2f7019bca10a25
jasveer singh <jasveersinghok@gmail.com>
2024-02-01 17:31:33 +0530

    first commit

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -21,22 +21,16 @@ const Projects = ({openModal,setOpenModal}) => {
             <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
           }
           <Divider />
-          {toggle === 'web app' ?
-            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+          {toggle === 'Front-End' ?
+            <ToggleButton active value="web app" onClick={() => setToggle('Front-End')}>Front-End</ToggleButton>
             :
-            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+            <ToggleButton value="web app" onClick={() => setToggle('Front-End')}>Front-End</ToggleButton>
           }
           <Divider />
-          {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+          {toggle === 'Full stack' ?
+            <ToggleButton active value="android app" onClick={() => setToggle('Full stack')}>Full Stack</ToggleButton>
             :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-          }
-          <Divider />
-          {toggle === 'machine learning' ?
-            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-            :
-            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            <ToggleButton value="android app" onClick={() => setToggle('Full stack')}>Full Stack</ToggleButton>
           }
         </ToggleButtonGroup>
         <CardContainer>
